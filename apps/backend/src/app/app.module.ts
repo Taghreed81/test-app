d5a56073605f40a40f1ef3a828fbb04bd5b88b0c
Taghreed <tagrid21003@hotmail.com>
2023-07-10 15:06:49 +0300

    connected to database

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
+import { DataSource } from 'typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5432,
+      "username": "taghreed",
+      "password": "1234",
+      "database": "test",
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ apps/backend/src/user/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+export class CreateUserDto {
+
+    readonly firstname: string;
+
+    readonly lastname: string;
+
+    readonly username: string;
+
+    readonly email: string;
+
+    readonly password: string;
+}
